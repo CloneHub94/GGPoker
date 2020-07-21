80774135792f3a8cc9f94648394c2204cc458f62
Ahmet <ahmetabdi@gmail.com>
2020-07-21 12:07:04 +0100

    Refactor

--- Core.cpp
+++ Core.cpp
@@ -4,16 +4,16 @@ namespace Green
 {
 	Core::Core()
 	{
-		auto m_AppModuleStr = skCrypt("55 8B EC 51 A1 ? ? ? ? 85 C0 74 ? 8B E5 5D C3 68 ? ? ? ?");
-		auto m_AppModuleFunc = Signature(m_AppModuleStr).Scan().As<uint64_t*>();
-		m_AppModuleStr.clear();
+		auto AppModuleStr = skCrypt("55 8B EC 51 A1 ? ? ? ? 85 C0 74 ? 8B E5 5D C3 68 ? ? ? ?");
+		auto AppModuleFuncAddress = Signature(AppModuleStr).Scan().As<uint64_t*>();
+		AppModuleStr.clear();
 
-		g_Logger->Info("AppModule function %X", m_AppModuleFunc);
+		g_Logger->Info("AppModule function %X", AppModuleFuncAddress);
 
 		typedef int(__stdcall* AppModuleUnkFunc_)(void*);
-		AppModuleUnkFunc_ AppModuleFunc = (AppModuleUnkFunc_)m_AppModuleFunc;
+		AppModuleUnkFunc_ AppModuleFunc = (AppModuleUnkFunc_)AppModuleFuncAddress;
 
-		auto AppModule = AppModuleFunc(NULL); // Call the function
+		auto AppModule = AppModuleFunc(NULL); // Call the function which returns AppModule pointer
 
 		g_Logger->Info("AppModule %X", AppModule);
 	}
